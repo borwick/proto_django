687110bda2b62325ba406f2e8634603aaba82500
John Borwick <john_borwick@pobox.com>
2014-10-07 21:26:10 -0400

    Fixed for new pip list output
    `pip list` used to return `package==version`. Now it returns
    `package (version)`.

--- requirements/unseen.py
+++ requirements/unseen.py
@@ -1,11 +1,27 @@
 #!python
 from glob import glob
+import re
 import sys
 
 SEEN_REQUIREMENTS = set()
+
+PIP_EQUALS_STRING = re.compile("""
+^
+    ([^=]+)   # not equals sign characters in 1
+    ==
+    (.*)      # version in 2
+""",
+                               re.VERBOSE)
+
 def add_requirements(fh):
     for line in fh:
-        SEEN_REQUIREMENTS.add(line.strip())
+        equals_match = PIP_EQUALS_STRING.match(line)
+        if equals_match:
+            paren_string = '{} ({})'.format(equals_match.group(1),
+                                            equals_match.group(2))
+            SEEN_REQUIREMENTS.add(paren_string)
+        else:
+            SEEN_REQUIREMENTS.add(line.strip())
 
 
 if __name__=='__main__':
